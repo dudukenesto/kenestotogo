6e027aeefdbb5fe0efc0975dfee57e610763fdd5
glutik <glutik@gmail.com>
2016-08-17 14:30:39 +0300

    toasts cleanup

--- app/components/Documents.js
+++ app/components/Documents.js
@@ -223,12 +223,12 @@ class Documents extends Component {
     const {documentlists} = this.props
     var documentlist = getDocumentsContext(this.props);
     const hasError = documentlist.catId in documentlists ? documentlists[documentlist.catId].hasError : false;
-    // const errorMessage = documentlist.catId in documentlists ? documentlists[documentlist.catId].errorMessage : "";
-    const errorMessage = "Luk, I am your father!";
+    const errorMessage = documentlist.catId in documentlists ? documentlists[documentlist.catId].errorMessage : "";
+    
 
-    // if (hasError && this.refs.masterView != undefined) {
+    if (hasError && this.refs.masterView != undefined) {
       this.refs.masterView.showMessage("success", errorMessage);
-    // }
+    }
   }
 
   openModal(){
