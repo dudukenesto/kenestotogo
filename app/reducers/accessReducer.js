2a246053da8248d2ea115db64babaa7bb400dac6
adambmc <adam.bmc@gmail.com>
2016-09-05 13:58:58 +0300

    left menu implementation

--- app/reducers/accessReducer.js
+++ app/reducers/accessReducer.js
@@ -1,54 +1,68 @@
 import * as types from '../constants/ActionTypes'
 
-function accessReducer (state = { isLoggedIn : false, token: "" , env : 'dev', isFetching : false}, action) {
+function accessReducer(state =
+  {
+    isLoggedIn: false,
+    token: "",
+    env: 'dev',
+    isFetching: false,
+    email: "",
+    firstName: "",
+    lastName: "",
+    thumbnailPath: ""
+  }, action) {
   switch (action.type) {
-    case types.UPDATE_IS_FETCHING: 
-        return {
-          ...state, 
-          isFetching : action.isFetching
-        }
-     case types.SUBMIT_ERROR: 
-        return {
-          ...state, 
-          isFetching : false, 
-          HasError: true, 
-          GlobalErrorMessage: action.message
-        }
-     case types.SUBMIT_SUCCESS: 
-        return {
-          ...state, 
-          isFetching : false, 
-          HasError: false, 
-          GlobalErrorMessage: "", 
-          GlobalSuccessMessage: action.messge
-        }
+    case types.UPDATE_IS_FETCHING:
+      return {
+          ...state,
+        isFetching: action.isFetching
+      }
+    case types.SUBMIT_ERROR:
+      return {
+          ...state,
+        isFetching: false,
+        HasError: true,
+        GlobalErrorMessage: action.message
+      }
+    case types.SUBMIT_SUCCESS:
+      return {
+          ...state,
+        isFetching: false,
+        HasError: false,
+        GlobalErrorMessage: "",
+        GlobalSuccessMessage: action.messge
+      }
     case types.SetEnv:
-        return {
+      return {
           ...state,
-          env: action.env
-        }
-        case types.PASSWORD_SENT:
-        return {
+        env: action.env
+      }
+    case types.PASSWORD_SENT:
+      return {
           ...state,
-          passwordSent: action.sent,
-          isFetching: false
-        }
+        passwordSent: action.sent,
+        isFetching: false
+      }
     case types.LOGIN: {
-        return{
-            ...state, 
-            accessToken: action.accessToken
-        }
-       
+      return {
+            ...state,
+        accessToken: action.accessToken
+      }
+
     }
     case types.UPDATE_LOGIN_INFO: {
-        return{
-            ...state, 
-            isLoggedIn: action.isLoggedIn, 
-            sessionToken: action.sessionToken, 
-            isFetching: false,
-            env: action.env
-        }
-       
+      return {
+            ...state,
+        isLoggedIn: action.isLoggedIn,
+        sessionToken: action.sessionToken,
+        isFetching: false,
+        env: action.env,
+        email: action.email,
+        firstName: action.firstName,
+        lastName: action.lastName,
+        thumbnailPath: action.thumbnailPath
+      }
+
     }
 
     default:
