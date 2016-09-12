f0263700b081f1e8fb1146d881486e1ac160d74a
dudukenesto <dudu@kenesto.com>
2016-09-12 13:35:01 +0300

    upload files exmperiments

--- android/app/src/main/java/com/kenestotogo/MainApplication.java
+++ android/app/src/main/java/com/kenestotogo/MainApplication.java
@@ -2,8 +2,8 @@ package com.kenestotogo;
 
 import android.app.Application;
 import android.util.Log;
-
 import com.facebook.react.ReactApplication;
+import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -14,6 +14,8 @@ import java.util.List;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.github.yamill.orientation.OrientationPackage;
 import com.github.alinz.reactnativewebviewbridge.WebViewBridgePackage;
+import com.fileopener.FileOpenerPackage;
+import com.rnfs.RNFSPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -27,10 +29,13 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-          new OrientationPackage()
-        , new VectorIconsPackage()
-        , new ReactMaterialKitPackage(),
-        new WebViewBridgePackage() 
+            new PickerPackage(),
+          new OrientationPackage(),
+         new VectorIconsPackage(),
+         new ReactMaterialKitPackage(),
+        new WebViewBridgePackage() ,
+        new FileOpenerPackage(),
+         new RNFSPackage() 
 
       );
     }
