4cc54b3470f02c3a7c344032532f3110c78f94b4
dudukenesto <dudu@kenesto.com>
2016-10-19 19:12:29 +0300

    fix merge shit -

--- app/components/AddPeople11.js
+++ app/components/AddPeople11.js
@@ -2,103 +2,300 @@
 // var React = require('react');
 import React, {Component} from 'react';
 import KenestoTagAutocomplete from './KenestoTagAutocomplete';
+import DropDownTrigger from './DropDownTrigger';
 import ViewContainer from '../components/ViewContainer';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import {connect} from 'react-redux'
 import * as peopleAcions from '../actions/peopleActions'
-import {getSelectedDocument} from '../utils/documentsUtils'
+import {getSelectedDocument} from '../utils/documentsUtils';
+import ProgressBar from './ProgressBar'
 var ReactNative = require('react-native');
-
+import * as navActions from '../actions/navActions'
+import * as docActions from '../actions/documentlists'
 var {
   View,
   Text,
-  // TextInput,
   StyleSheet,
   ScrollView,
-  // TouchableOpacity,
-  // Dimensions,
-  // TouchableWithoutFeedback
+  TouchableWithoutFeedback,
+  Image,
+  Dimensions
 } = ReactNative;
-// import Button from './Button'
+var {height, width} = Dimensions.get('window');
+var Orientation = require('./KenestoDeviceOrientation');
+
 import ProggressBar from "../components/ProgressBar";
-// import _ from 'lodash';
-// import TagInput from 'react-native-taginput';
 
-// var HEADER = '#3b5998';
-// var BGWASH = 'rgba(255,255,255,0.8)';
-// var DISABLED_WASH = 'rgba(255,255,255,0.25)';
-
-var suggestions = [
-    '1scott@kenestodemo.com',
-    '2lisa@kenestodemo.com',
-    '3jeff@kenestodemo.com',
-    '4joe@kenestodemo.com',
-    '5admin@kenestodemo.com',
-    '6asd@asa.sd',
-    '7sdf@asa.sd',
-    '8ghjkl@asa.sd',
-    '9rty@asa.sd',
-    '10wer@asa.sd',
-    '11wer@zxa.sd',
-    '12wer@zxa.sd'
-    ]
-class AddPeople extends Component {
+var globalOptions = [
+  {
+    name: "VIEW_ONLY",
+    icon: "remove-red-eye",
+    title: "View only",
   
+  },
+  {
+    name: "ALLOW_DOWNLOAD",
+    icon: "file-download",
+    title: "Download"
+  },
+  {
+    name: "ALLOW_UPDATE_VERSIONS",
+    icon: "update",
+    title: "Update"
+  }
+];
+
+var userOptions = [
+  {
+    name: "VIEW_ONLY",
+    icon: "remove-red-eye",
+    title: "View only"
+  },
+  {
+    name: "ALLOW_DOWNLOAD",
+    icon: "file-download",
+    title: "Download"
+  },
+  {
+    name: "ALLOW_UPDATE_VERSIONS",
+    icon: "update",
+    title: "Update"
+  },
+  {
+    name: "NONE",
+    icon: "not-interested",
+    title: "Delete share"
+  }
+]
+
+class AddPeople extends Component {
+
   constructor(props) {
     super(props)
 
-        this.state = {
-          showSharingList: true
-        };
+    this.state = {
+      showSharingList: true,
+      globalPermissions: 'VIEW_ONLY',
+      orientation: Orientation.getInitialOrientation()
+    };
   }
-  
-  _onChange(){
-    
+
+  componentDidMount() {
+    var tags = [];
+    this.orientationListener = Orientation.addOrientationListener(this._orientationDidChange.bind(this));
+    this.props.dispatch(docActions.SetSharingPermissions(tags));
   }
-  
-  _onUpdateLayout(){
-    
+
+  componentWillUnmount() {
+    this.orientationListener.remove();
+    Orientation.removeOrientationListener();
   }
-  
-  _onUpdateTags(){
-    
+
+  _orientationDidChange(orientation) {
+    this.setState({
+      orientation: orientation == 'LANDSCAPE' ? 'LANDSCAPE' : 'PORTRAIT'
+    })
   }
-  
-  showSharingList(){
-    this.setState({showSharingList: true})
+
+  _onChange(tags) {
+      this.props.dispatch(docActions.SetSharingPermissions(tags));
   }
-  
-  hideSharingList(){
-    this.setState({showSharingList: false})
+
+  _onUpdateLayout() {
+
   }
-  
-  formatNewTag(tag){      
+
+  _onUpdateTags() {
+
+  }
+
+  showSharingList() {
+    this.setState({ showSharingList: true })
+  }
+
+  hideSharingList() {
+    this.setState({ showSharingList: false })
+  }
+
+  formatNewTag(tag) {
     // trim spaces, check for email correct format etc. 
     // If the formatted text is not a correct email - return false
     tag = tag.trim();
     return /\S+@\S+\.\S+/.test(tag) ? tag : false;
   }
-  
-  onErrorAddNewTag(tag){     // show error message/toast
+
+  onErrorAddNewTag(tag) {     // show error message/toast
     var errorMessage = tag + ' is not a valid email!'
-    this.refs.mainContainer.showMessage("info", errorMessage);
+    //this.refs.mainContainer2.showMessage("info", errorMessage);
+    this.props.dispatch(navActions.emitToast("info", "", errorMessage));
   }
-  
-  submitSelectedTags(){
+
+  submitSelectedTags() {
     // VALIDATE TAGS, COMBINE this.state.selected WITH PERMISSIONS AND SEND TO SERVER
   }
 
+  componentWillMount() {
+    //  alert(JSON.stringify(this.props.navReducer));
+    var document = getSelectedDocument(this.props.documentlists, this.props.navReducer);
+    this.setState({ document: document });
 
+    this.props.dispatch(peopleAcions.RequestShareObjectInfo(document.Id, document.FamilyCode, document.Name));
+    //alert(document)
+    // alert('document = ' + document.Name)
+    //  alert(this.props.UsersAndGroups)
+
+
+  }
+
+  // EXAMPLE OF CUSTOM AUTOCOMPLETE ROW TEMPLATE. rowData INCLUDES ALL FIELDS.
+  // 
+  getAutocompleteRowTemplate(searchedtext, iconType, iconName, rowData) {   
+    
+    return (
+      <View style={styles.autocompleteRow}>
+        {searchedtext}
+        <View style={[styles.roundIcon, {marginLeft: 10}]}>
+            {iconType == 'icon' ? <Icon name={iconName} style={styles.iconMedium} /> : <Image source = {{ uri: iconName }} style={styles.thumbnail} />}
+          </View>
+      </View>
+    )
+  }
+
+  // EXAMPLE OF CUSTOM TAG TEMPLATE
+  // 
+  getTagTemplate(tag, removeTag) {
+    
+    return (
+      <View style={styles.tagInnerContainer}>
+        <View style={[styles.roundIcon, { marginLeft: -5.5 }]}>
+          {tag.iconType == 'icon' ? <Icon name={tag.iconName} style={styles.iconMedium} /> : <Image source = {{ uri: tag.iconName }} style={styles.thumbnail} />}
+        </View>
+        <View style={{ flex: 1 }}><Text style={styles.tagText} numberOfLines={1}>{tag.tagName}</Text></View>
+        <Icon name="close" style={styles.closeIcon} onPress={removeTag.bind(this, tag)} />
+      </View>
+    )
+  }
+
+
+  renderPermissionsTrigger(permissionName : string, isFetching: boolean) {
+
+
+    var iconName;
+    switch (permissionName) {
+      case "VIEW_ONLY":
+        iconName = "remove-red-eye";
+        break;
+      case "ALLOW_DOWNLOAD":
+        iconName = "file-download";
+        break;
+      case "ALLOW_UPDATE_VERSIONS":
+        iconName = "update";
+        break;
+    }
+
+    const content = isFetching? <View style={styles.creatingFolder}><ProgressBar isLoading={true}/></View> :  <Icon name={iconName} style={styles.icon} />; 
+
+    return (
+      <View style={styles.dropDownTriggerTemplateContainer}>
+        <View style={styles.dropDownTriggerTemplate}>
+         {content}
+          <Icon name="keyboard-arrow-down" style={[styles.icon, styles.iconDown]} />
+        </View>
+      </View>
+    )
+  }
+
+ onMenuSelect(name: string){
+ 
+    
+
+     if (this.props.clickedTrigger != 'addPeopleTrigger')
+     {
+       this.props.dispatch(navActions.requestUpdateTrigger(name));
+       this.props.dispatch(docActions.UpdateDocumentSharingPermission());
+       
+     }
+     else
+       this.props.dispatch(navActions.updatedSelectedTrigerValue(name));
+       
+ }
+ 
+
+  renderOptionTemplate(rowData) {
+    return (
+      <View>
+      <TouchableWithoutFeedback onPress={() => (this.onMenuSelect(rowData.name))}>
+      <View style={styles.optionRow}>
+    
+        
+            <Icon name={rowData.icon} style={styles.icon} />
+            <Text style={styles.optionTitle}>{rowData.title}</Text>
+     
+      </View>
+         </TouchableWithoutFeedback>
+         </View>
+    )
+  }
+
+
+  renderCurrentPermissions() {
+
+    var permissions = this.props.ObjectInfo.UsersPermissions.map(function (permission) {
+
+      var iconName;
+      var uri = 'https://upload.wikimedia.org/wikipedia/commons/f/f5/Poster-sized_portrait_of_Barack_Obama.jpg'
+      if (!permission.ThumbnailPath) {
+        if (permission.IsGroup) {
+          iconName = 'group';
+        }
+        else {
+          iconName = permission.IsExternal ? 'person-outline' : 'person'
+        }
+      }
+      var textWidth = (this.state.orientation == 'PORTRAIT') ? width - 140 : height - 140;
+      
+      return (
+        <View style={styles.sharingRow} key={permission.UserId}>
+          <View style={styles.roundIcon}>
+            {!permission.ThumbnailPath ? <Icon name={iconName} style={styles.iconMedium} /> : <Image source = {{ uri: uri }} style={styles.thumbnail} />}
+          </View>
+          <View style={{ maxWidth: textWidth }}>
+            <Text key={permission.ParticipantUniqueID} numberOfLines={1}>{permission.Name}</Text>
+          </View>
+          <DropDownTrigger
+            dropDownTriggerTemplate={this.renderPermissionsTrigger.bind(this) }
+            dropDownTriggerStyle={styles.dropDownTriggerStyle}
+            activeTriggerStyle={styles.activeTriggerStyle}
+            optionTemplate={this.renderOptionTemplate.bind(this) }
+            options={userOptions}
+            aligningOptionsWithTrigger={"right"}
+            openingDirection={"down"}
+            selected={permission.PermissionType}
+            id= {'trigger_' + permission.ParticipantUniqueID}
+            />
+        </View>
+
+      );
+    }, this);
+
+    return permissions
+
+  }
 
   render() {
 
-
-    
+    if (this.props.isFetching) {
+      return (<View style={styles.creatingFolder}>
+        <ProgressBar isLoading={true}/>
+      </View>
+      )
+    }
     return (
       <ViewContainer ref="mainContainer">
         <View style={styles.container}>
           <KenestoTagAutocomplete
             ref='tagInput'
-            suggestions={suggestions}
+            suggestions={this.props.UsersAndGroups}
             containerStyle={styles.autocompleteContainer}
             inputContainerStyle={styles.tagsInputContainer}
             listStyle={styles.listStyle}
@@ -106,6 +303,7 @@ class AddPeople extends Component {
             newTagStyle={styles.newTagStyle}
             rowContainerStyle={styles.rowContainerStyle}
             autocompleteTextStyle={styles.autocompleteTextStyle}
+            tagContainerStyle={styles.tagContainerStyle}
             onChange={this._onChange.bind(this) }
             onUpdateLayout={this._onUpdateLayout.bind(this) }
             onUpdateTags={this._onUpdateTags.bind(this) }
@@ -118,52 +316,46 @@ class AddPeople extends Component {
             addNewTagTitle={"Add a new user: "}
             formatNewTag={this.formatNewTag.bind(this) }
             onErrorAddNewTag={this.onErrorAddNewTag.bind(this) }
-            // autocompleteField={"email"}
+            autocompleteRowTemplate={this.getAutocompleteRowTemplate.bind(this) }
+            tagTemplate={this.getTagTemplate.bind(this)}
+            autocompleteField={"Name"}
+            uniqueField={"ParticipantUniqueID"}
+            aditionalField={"FamilyCode"}
+            />
+
+          <DropDownTrigger
+            dropDownTriggerTemplate={this.renderPermissionsTrigger.bind(this) }
+            dropDownTriggerStyle={styles.dropDownGlobalTriggerStyle}
+            dropDownTriggerContainer={styles.dropDownTriggerContainer}
+            activeTriggerStyle={styles.activeTriggerStyle}
+            optionTemplate={this.renderOptionTemplate.bind(this) }
+          
+            options={globalOptions}
+            aligningOptionsWithTrigger={"right"}
+            openingDirection={"down"}
+            selected={this.state.globalPermissions}
+            id="addPeopleTrigger"
             />
 
           {this.state.showSharingList == true ?
             <ScrollView keyboardShouldPersistTaps={true} showsVerticalScrollIndicator={false}>
               <View style={{ flex: 1 }}>
-                <View>
-                  <Text>Sharing</Text>
+                <View style={styles.sharingTitleContainer}>
+                  <Text style={styles.sharingTitle}>Sharing</Text>
                 </View>
 
                 <View>
-                  <Text>1 list of people</Text>
-                  <Text>list of people</Text>
-                  <Text>list of people</Text>
-                  <Text>list of people</Text>
-                  <Text>5 list of people</Text>
-                  <Text>list of people</Text>
-                  <Text>list of people</Text>
-                  <Text>list of people</Text>
-                  <Text>list of people</Text>
-                  <Text>10 list of people</Text>
-                  <Text>list of people</Text>
-                  <Text>list of people</Text>
-                  <Text>list of people</Text>
-                  <Text>list of people</Text>
-                  <Text>15 list of people</Text>
-                  <Text>list of people</Text>
-                  <Text>list of people</Text>
-                  <Text>list of people</Text>
-                  <Text>list of people</Text>
-                  <Text>20 list of people</Text>
-                  <Text>list of people</Text>
-                  <Text>list of people</Text>
-                  <Text>list of people</Text>
-                  <Text>24 list of people</Text>
+                  {this.renderCurrentPermissions.bind(this)() }
                 </View>
               </View>
             </ScrollView>
             :
             <View style={{}}></View>
-            
           }
 
-        </View>      
+        </View>
       </ViewContainer>
-      
+
     );
   }
 }
@@ -171,7 +363,7 @@ class AddPeople extends Component {
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',   
+    backgroundColor: '#fff',
   },
   autocompleteContainer: {
 
@@ -182,6 +374,137 @@ var styles = StyleSheet.create({
   newTagStyle: {},
   rowContainerStyle: {},
   autocompleteTextStyle: {},
+  tagContainerStyle: {},
+  sharingTitleContainer: {
+    height: 45,
+    backgroundColor: '#F5F6F8',
+    justifyContent: 'center',
+    paddingLeft: 25
+  },
+  sharingTitle: {
+    fontWeight: '400',
+    color: '#000',
+  },
+  icon: {
+    fontSize: 15,
+    color: '#999',
+    paddingLeft: 3,
+    paddingRight: 3
+  },
+  iconDown: {
+    fontSize: 18,
+    paddingRight: 0,
+    color: "#000"
+  },
+  iconMedium: {
+    fontSize: 22,
+    color: "#999",
+    borderWidth: 1,
+    marginLeft: 2
+  },
+  roundIcon: {
+    borderWidth: 0.5,
+    borderColor: "#999",
+    borderRadius: 16,
+    width: 31,
+    height: 31,
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: 7,
+    // overflow: "hidden",
+    zIndex: 100
+  },
+  dropDownTriggerStyle: {
+    borderColor: "transparent"
+  },
+  dropDownGlobalTriggerStyle: {
+    borderColor: "#ccc"
+  },
+  dropDownTriggerContainer: {
+    position: 'absolute',
+    top: 13,
+    right: 0,
+  },
+  activeTriggerStyle: {
+    borderColor: "#000"
+  },
+  dropDownTriggerTemplateContainer: {
+    flex: 1,
+    alignItems: "center",
+  },
+  dropDownTriggerTemplate: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  sharingRow: {
+    flexDirection: "row",
+    height: 50,
+    alignItems: "center",
+    marginLeft: 30,
+  },
+  thumbnail: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+  },
+  optionRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    height: 45,
+    paddingHorizontal: 12,
+  },
+  optionTitle: {
+    paddingLeft: 8
+  },
+  autocompleteRow: { 
+    flexDirection: "row", 
+    alignItems: "center", 
+    flex: 1, 
+    justifyContent: "space-between" 
+  },
+  tagInnerContainer: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  userIcon: {
+    fontSize: 34,
+    left: -7,
+  },
+  tagText: {
+    fontSize: 14,
+    color: '#000',
+    padding: 0,
+    margin: 0
+  },
+  closeIcon: {
+    fontSize: 15,
+    marginTop: 2,
+    marginRight: -7,
+    padding: 6,
+  },
+  avatar: {
+    height: 30,
+    width: 30,
+    borderRadius: 15,
+  },
+
 });
 
+function mapStateToProps(state) {
 
+  const { peopleReducer, documentlists, navReducer  } = state
+
+
+  return {
+    isFetching: peopleReducer.isFetching,
+    documentlists,
+    navReducer,
+    ObjectInfo: peopleReducer.ObjectInfo,
+    UsersAndGroups: peopleReducer.UsersAndGroups,
+    clickedTrigger: navReducer.clickedTrigger,
+  }
+}
+
+export default connect(mapStateToProps)(AddPeople)
