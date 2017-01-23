8cdcd40251aae3f73b04c76c7a4febcaf0cb1099
dudukenesto <dudu@kenesto.com>
2017-01-23 16:52:16 +0200

    fix

--- app/components/ItemMenu.js
+++ app/components/ItemMenu.js
@@ -287,8 +287,8 @@ class ItemMenu extends React.Component {
 
     _renderUpdateVersionsAction(document) {
         if (this.state.document.IsOwnedByRequestor || 
-             this.props.documentsReducer.selectedObject.permissions.AllowUpdateVersions 
-             && (this.state.document.IsCheckedOut || !this.state.document.isVault)) {
+             (this.props.documentsReducer.selectedObject.permissions.AllowUpdateVersions 
+        && (this.state.document.IsCheckedOut || !this.state.document.isVault) )) {
             return (<TouchableHighlight onPress={this.updateVersions.bind(this)} underlayColor="#E9EAEC">
                 <View style={styles.actionHolder}>
                     <Icon name="update" style={styles.icon} />
