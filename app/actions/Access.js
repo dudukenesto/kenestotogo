8422e36cd0786768abbe589c01e537b39eee4aa7
dudukenesto <dudu@kenesto.com>
2017-02-16 10:48:37 +0200

    encode user name and password

--- app/actions/Access.js
+++ app/actions/Access.js
@@ -211,7 +211,7 @@ export function login(userId : string, password: string, env: string = 'dev')  {
 
       
    
-        var authUrl = getAuthUrl(env,userId, password, constans.ACCESS_KEY);
+        var authUrl = getAuthUrl(env,stricturiEncode(userId), stricturiEncode(password), constans.ACCESS_KEY);
         return fetch(authUrl)
             .then((response) => response.json())
             .catch((error) => {
